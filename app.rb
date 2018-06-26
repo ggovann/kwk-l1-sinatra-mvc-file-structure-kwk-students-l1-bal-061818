de9ff438221be7ae60e54cd2d908b8fd4d12a932
Grace Govan <ggovan11@outlook.com>
2018-06-26 17:47:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,11 +19,11 @@ class App < Sinatra::Base
      @number.times do
     end 
 
-    get '/say/:word1/:word2/:word3/:word4/:word5'
+    get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
     end
 
-get '/:operation/:number1/:number2'
+get '/:operation/:number1/:number2' do
 @operation = params[:operation]
 @number1 = params[:number1]
 @number2 = params[:number2]
